4c7363c4c9876a4ac40fa7f3ef8ccc9f4c7f0d83
Josip Harci <harcijosip99@gmail.com>
2022-12-19 23:34:04 +0100

    Update
    Add Nav

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,8 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-           <div class="LogoClass">
-               <img class="logo" src={logo} alt="logo" />
+           <div class="MenuClass">
+              <div class="LogoClass">
+                  <img class="logo" src={logo} alt="logo" />
+              </div>
+               <ul class="Menu">
+                  <li class="Menu-item"><a href="#" class="link">Naslovnica</a></li>
+                  <li class="Menu-item"><a href="#" class="link">O meni</a></li>
+                  <li class="Menu-item"><a href="#" class="link">Kontakt</a></li>
+               </ul>
            </div>
       </header>
     </div>
