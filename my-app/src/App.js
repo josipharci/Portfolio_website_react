ecb739020be5ea25a28bc14721f03e55ab705147
Josip Harci <harcijosip99@gmail.com>
2022-12-22 23:07:10 +0100

    fix

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,19 @@
 
 import Navbar from './Navbar';
-import CarouselItem from './CarouselItem';
+import Carousel from './CarouselItem';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
+
 
 function App() {
   return (
+    
     <div className="App">
       <Navbar />
-      
+      <Carousel/>
    
     </div>
   );
 }
 
 export default App;
+
--- my-app/src/CarouselItem.js
+++ my-app/src/CarouselItem.js
@@ -1,11 +1,51 @@
 
-import React from 'react';
-import logo from './logo.png';
+import React, {useState} from "react"; 
+import Carousel from 'react-bootstrap/Carousel';
 
-function CarouselItem(){
-    
-             <img src={logo} class="d-block w-100" alt="Logo"/>
-     
+
+const data = [
+  {
+   image: require('./images/image1.jpg'), 
+   caption:"Caption",
+   description:"Description Here"
+  },
+  {
+    image:require('./images/image2.jpg'), 
+    caption:"Caption",
+    description:"Description Here"
+   },
+   {
+    image:require('./images/image3.jpg'), 
+    caption:"Caption",
+    description:"Description Here"
+   } 
+]
+
+function HomeCarousel() {
+  const [index, setIndex] = useState(0);
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+       {data.map((slide, i) => {
+        return (
+          <Carousel.Item>        
+        <img
+          className="d-block w-100"
+          src={slide.image}
+          alt="slider image"
+        />
+        <Carousel.Caption>
+          <h3>{slide.caption}</h3>
+          <p>{slide.description}</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+        )
+      })}
+      
+    </Carousel>
+  );
 }
 
-export default CarouselItem;
