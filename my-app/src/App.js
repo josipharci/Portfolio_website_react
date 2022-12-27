1cdda398e291744d783140959a6e21bcbdfb7790
Josip Harci <harcijosip99@gmail.com>
2022-12-27 23:06:19 +0100

    Restarted

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,25 @@
-
-import Navbar from './Navbar';
-import CarouselItem  from './CarouselItem';
-import './css/App.css';
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    
     <div className="App">
-      <Navbar />
-      <CarouselItem/>
-   
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
 export default App;
-
