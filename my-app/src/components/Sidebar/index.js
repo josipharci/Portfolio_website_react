0e1fdb4adee68bc20b965a4e604147f431fd0689
Josip Harci <harcijosip99@gmail.com>
2022-12-29 22:26:25 +0100

    Update Sidebar

--- my-app/src/components/Sidebar/index.js
+++ my-app/src/components/Sidebar/index.js
@@ -1,5 +1,13 @@
+import { Link } from 'react-router-dom';
 import './index.scss';
 
-const Sidebar = () => <>Hello form slidbar</>
+const Sidebar = () => (
+    <div className='nav-bar'>
+        <h1>Pozdrav</h1>
+        <Link>
+            <img />
+        </Link>
+    </div>
+)
 
 export default Sidebar;
