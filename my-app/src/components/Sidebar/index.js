6cc46ae51088922d875df92252176ac0fe02f14d
Josip Harci <harcijosip99@gmail.com>
2022-12-31 10:13:46 +0100

    Update

--- my-app/src/components/Sidebar/index.js
+++ my-app/src/components/Sidebar/index.js
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom';
 import './index.scss';
 import Logo from '../../assets/images/logo.png';
+import LogoSublime from '../../assets/images/logo-sub.png';
 
 
 const Sidebar = () => (
     <div className='nav-bar'>
         <Link className='Logo' to='/'>
-            <img src={Logo} alt="LOGO"/>
+            <img className='Logo-img'src={Logo} alt="LOGO"/>
+            <img className='Sub-logo' src={LogoSublime} alt="JOSIP"/>
         </Link>
     </div>
 )
