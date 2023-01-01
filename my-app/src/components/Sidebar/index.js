68e25ccc1f1aef00d82315b7eb34113bfc1f144d
Josip Harci <harcijosip99@gmail.com>
2023-01-01 15:30:59 +0100

    Update

--- my-app/src/components/Sidebar/index.js
+++ my-app/src/components/Sidebar/index.js
@@ -4,6 +4,7 @@ import Logo from '../../assets/images/logo.png';
 import LogoSublime from '../../assets/images/logo-sub.png';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faHome , faDiagramProject , faUser , faEnvelope} from '@fortawesome/free-solid-svg-icons';
+import {faFacebook, faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
 
 
 const Sidebar = () => (
@@ -25,6 +26,29 @@ const Sidebar = () => (
         <NavLink className='Links' exact='true' activeclassname='active' to='/'>
             <FontAwesomeIcon icon={faEnvelope} color='#fff'/>
         </NavLink>
+        </div>
+        <div className='SLinks'>
+        <a className='S_link'
+           target="_blank"
+           rel='noreferrer'
+           href='https://www.linkedin.com/in/josip-harci-9063331b1/'
+        >
+            <FontAwesomeIcon icon={faLinkedin} color='#fff'/>
+        </a>  
+        <a className='S_link Github_i'
+           target="_blank"
+           rel='noreferrer'
+           href='https://github.com/josipharci'
+        >
+            <FontAwesomeIcon icon={faGithub} color='#fff'/>
+        </a>
+        <a className='S_link' 
+           target="_blank"
+           rel='noreferrer'
+           href='https://www.facebook.com/harci99'
+        >
+            <FontAwesomeIcon icon={faFacebook} color='#fff'/>
+        </a>
         </div>
     </div>
 )
