7559be601ff12770aace73ac26add3454b4be2ca
Josip Harci <harcijosip99@gmail.com>
2023-01-01 14:54:33 +0100

    Update

--- my-app/src/components/Sidebar/index.js
+++ my-app/src/components/Sidebar/index.js
@@ -3,7 +3,7 @@ import './index.scss';
 import Logo from '../../assets/images/logo.png';
 import LogoSublime from '../../assets/images/logo-sub.png';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faHome} from '@fortawesome/free-solid-svg-icons';
+import {faHome , faDiagramProject , faUser , faEnvelope} from '@fortawesome/free-solid-svg-icons';
 
 
 const Sidebar = () => (
@@ -12,9 +12,20 @@ const Sidebar = () => (
             <img className='Logo-img'src={Logo} alt="LOGO"/>
             <img className='Sub-logo' src={LogoSublime} alt="JOSIP"/>
         </Link>
-        <NavLink exact='true' activeclassname='active' to='/'>
+        <div className='Links_box'>
+        <NavLink className='Links' exact='true' activeclassname='active' to='/'>
             <FontAwesomeIcon icon={faHome} color='#fff'/>
         </NavLink>
+        <NavLink className='Links' exact='true' activeclassname='active' to='/'>
+            <FontAwesomeIcon icon={faDiagramProject} color='#fff'/>
+        </NavLink>
+        <NavLink className='Links' exact='true' activeclassname='active' to='/'>
+            <FontAwesomeIcon icon={faUser} color='#fff'/>
+        </NavLink>
+        <NavLink className='Links' exact='true' activeclassname='active' to='/'>
+            <FontAwesomeIcon icon={faEnvelope} color='#fff'/>
+        </NavLink>
+        </div>
     </div>
 )
 
