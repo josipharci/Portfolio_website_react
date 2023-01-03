5b61a681ae8ff6bd90097d3318077a87c219a61a
Josip Harci <harcijosip99@gmail.com>
2023-01-03 22:21:00 +0100

    Update

--- my-app/src/components/Home/index.js
+++ my-app/src/components/Home/index.js
@@ -1,9 +1,16 @@
 import './index.scss';
 
+
 const Home = () => {
     return (
-      <div>
-        <h1>Pozdrav Svijete</h1>
+      <div className="Container">
+        <div className="Content_left">
+          <h2 className="Heading">Hi , <br/> My name is Josip Harci <br/> I am 23 years old and come from Osijek.<br/>I am a Front-end developer.</h2>
+          <a href="/About" className="Buttom">More</a>
+        </div>
+        <div className="Content_right">
+          
+        </div>
       </div>
     );
 }
