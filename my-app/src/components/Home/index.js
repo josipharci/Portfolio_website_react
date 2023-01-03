a8751c01eb6d6a29b08bff6162203701c428f994
Josip Harci <harcijosip99@gmail.com>
2023-01-03 23:08:33 +0100

    Update

--- my-app/src/components/Home/index.js
+++ my-app/src/components/Home/index.js
@@ -5,8 +5,9 @@ const Home = () => {
     return (
       <div className="Container">
         <div className="Content_left">
-          <h2 className="Heading">Hi , <br/> My name is Josip Harci <br/> I am 23 years old and come from Osijek.<br/>I am a Front-end developer.</h2>
-          <a href="/About" className="Buttom">More</a>
+          <h2 className="Heading">Hi, <br/>My name is Josip Harci. <br/>I am 23 years old. <br/>I come from Osijek.<br/>I am a Front-end developer.</h2>
+          <p className="Text">HTML - CSS(LESS/SASS) - JavaScript - React</p>
+          <a href="/About" className="Buttom">MORE...</a>
         </div>
         <div className="Content_right">
           
