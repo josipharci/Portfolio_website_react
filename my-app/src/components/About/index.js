7dd1ec867e838b33d1dbbad934ee8d48954ce892
Josip Harci <harcijosip99@gmail.com>
2023-01-04 22:27:37 +0100

    Update

--- my-app/src/components/About/index.js
+++ my-app/src/components/About/index.js
@@ -2,7 +2,13 @@ import './index.scss';
 
 const About = () => {
     return(
-        <p1>About me</p1>
+        <div className="Content_left_about">
+            <h2 className="Heading">About me</h2>
+            <p className="Text">
+            My name is Josip. I am from Osijek, Croatia. <br/>I am 23 years old. I finished for Front-end and Back-end Developer. 1 year of learning and improving skills.
+            </p>
+            <p className="Text_two"></p>
+        </div>
     );
 }
 
