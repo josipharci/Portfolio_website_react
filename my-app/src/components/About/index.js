b81b5a37ec49ae0f0723a93073f0b583adb422d2
Josip Harci <harcijosip99@gmail.com>
2023-01-04 23:20:59 +0100

    Update

--- my-app/src/components/About/index.js
+++ my-app/src/components/About/index.js
@@ -1,4 +1,8 @@
 import './index.scss';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faStar} from '@fortawesome/free-solid-svg-icons';
+
+
 
 const About = () => {
     return(
@@ -7,7 +11,23 @@ const About = () => {
             <p className="Text">
             My name is Josip. I am from Osijek, Croatia. <br/>I am 23 years old. I finished for Front-end and Back-end Developer. 1 year of learning and improving skills.
             </p>
-            <p className="Text_two"></p>
+            <div className="Text_two">
+                <h3>Skills</h3>
+                <p>HTML and CSS 
+                   <span className='Star active_s'><FontAwesomeIcon icon={faStar}></FontAwesomeIcon></span>
+                   <span className='Star active_s'><FontAwesomeIcon icon={faStar}></FontAwesomeIcon></span>
+                   <span className='Star active_s'><FontAwesomeIcon icon={faStar}></FontAwesomeIcon></span>
+                   <span className='Star'><FontAwesomeIcon icon={faStar}></FontAwesomeIcon></span>
+                   <span className='Star'><FontAwesomeIcon icon={faStar}></FontAwesomeIcon></span>
+                </p>
+                <p>JavaScript
+                   <span className='Star active_s'><FontAwesomeIcon icon={faStar}></FontAwesomeIcon></span>
+                   <span className='Star active_s'><FontAwesomeIcon icon={faStar}></FontAwesomeIcon></span>
+                   <span className='Star active_s'><FontAwesomeIcon icon={faStar}></FontAwesomeIcon></span>
+                   <span className='Star'><FontAwesomeIcon icon={faStar}></FontAwesomeIcon></span>
+                   <span className='Star'><FontAwesomeIcon icon={faStar}></FontAwesomeIcon></span>
+                </p>
+            </div>
         </div>
     );
 }
