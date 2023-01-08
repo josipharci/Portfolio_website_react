50f20d72423d401c70ba80bd5deced2ffb607452
Josip Harci <harcijosip99@gmail.com>
2023-01-08 21:01:19 +0100

    Update

--- my-app/src/components/Contact/index.js
+++ my-app/src/components/Contact/index.js
@@ -6,13 +6,14 @@ const Contact = () => {
             <h2 className="Header_contact">Contact</h2>
             <form>
                 <label for="Name">Name</label>
-                <input id="name" placeholder="Name"></input>
+                <input type="text" id="name" placeholder="Name"></input>
                 <label for="Surame">Surname</label>
-                <input id="surname" placeholder="Surname"></input>
+                <input type="text" id="surname" placeholder="Surname"></input>
                 <label for="E-mail">E-mail</label>
-                <input id="email" placeholder="Email"></input>
+                <input type="text" id="email" placeholder="Email"></input>
                 <label for="E-mail">Message</label>
                 <textarea placeholder="Message"></textarea>
+                <input type="submit" id="submit" value="Send"></input>
             </form>
         </div>
     
