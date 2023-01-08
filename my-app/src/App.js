b833d376b4735babae766a7362cd9d52766864b2
Josip Harci <harcijosip99@gmail.com>
2023-01-08 07:30:14 +0100

    Update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,7 @@ import Layout from './components/Layout';
 import Home from './components/Home';
 import About from './components/About';
 import Project from './components/Project';
+import Contact from './components/Contact';
 function App(){
   return (
     <>
@@ -13,6 +14,7 @@ function App(){
       <Route index element={<Home />}/>
       <Route path='About' element={<About />}/>
       <Route path='Project' element={<Project />}/>
+      <Route path='Contact' element={<Contact />}/>
       </Route>
     </Routes>
     </>
